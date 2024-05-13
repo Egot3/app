786d2e4420874a81c32c4fe9ac98667bd01fa79c
Egot3 <130480425+Egot3@users.noreply.github.com>
2024-05-13 16:28:06 +0300

    Add files via upload

--- app/app.py
+++ app/app.py
@@ -1,94 +1,180 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, jsonify,session
 from random import randint, choice
+import string
 import requests
+#from login_web_controller import login_controller
 
 app = Flask(__name__)
+#app.register_blueprint(login_controller) 
 
-answers=-1
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]0/'
+
 colOfPrim = 6
+answers=0
 
 # Главная страница с кнопкой
 @app.route('/')
 def index():
+    if 'username' not in session:
+        return render_template('login.html')
     return render_template('index.html')
 
-"""# Обработчик для кнопки получения данных
-@app.route('/get_data')
-def get_data():
-    # URL второго API, который мы будем вызывать
-    api_url = 'http://127.0.0.1:5001/random_data'
+@app.route('/accaunt',methods=['POST','GET'])
+def accaunt():
+    if request.method=='POST':
+        newData={
+            'newName':request.form['newname'],
+            'newPassword':request.form['newpassword'],
+            'mail':request.form['mail']+'@'+request.form['mailmail']
+        }
+        return redirect('/'), jsonify(newData)
+    username='имя'
+    password='пароль'
+    mail='mail@mail.com'
+    primResh=21      #затычки
+    mailmail='mail.com'
+    nmail=mail.replace('@'+mailmail,'')
+    prist=primResh//100
+    grad=primResh%100
+    return render_template('accaunt.html', name=username,password=password,primResh=primResh,grad=grad,prist=prist,mail=nmail,mailmail=mailmail)
+
+@app.route('/logun', methods=['POST','GET'])
+def logun():
     
-    # Отправляем GET запрос к другому API
+    username=request.form['username']
+    password=request.form['password']
+    return redirect(url_for('log',username=username,password=password))
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    choice = request.form['Level']
+    #print(f'Выбран вариант: {choice}')
+    znaks=['+','-','*','÷','^']
+    if choice=='hardLevel':
+        numsInPrim = randint(3,16)
+    elif choice=="normalLevel":
+        znaks.pop()
+        numsInPrim = randint(3,11)
+    else:
+        znaks=znaks[:-3]
+        numsInPrim = randint(3,5)
+    znaks=''.join(znaks)
+    return redirect(url_for(".display",numsInPrim=numsInPrim,znaki=znaks,answers=0,colOfPrim=6,truePrim='None'))
+
+
+@app.route('/YGT')
+def answer():
+    api_url = 'http://127.0.0.1:5000/primdata'
     response = requests.get(api_url)
-    
-    # Получаем JSON данные
-    data = response.json()
-    
-    # Перенаправляем на другую страницу и передаем данные в URL
-    return redirect(url_for('display_data', temperature=data['temperature'], date=data['date']))"""
+    return render_template('answer.html',goodAnswers=response.json()['answers'])
+
 
 # Страница для отображения данных
-@app.route('/easyLevel/', methods=['get'])
-def easyLevel():
-    global answers, colOfPrim
-    if colOfPrim==0:
-        print(request.form.get("superAnswer"))
-        return render_template('answer.html',answer=request.form.get("superAnswer"))
-    answers+=1
-    colOfPrim-=1
-    numsInPrim = randint(3,5)
+@app.route('/trueDisplay/<numsInPrim>/<znaki>/<colOfPrim>/<truePrim>',methods=["GET",'POST'])
+def display(numsInPrim,znaki,colOfPrim,truePrim):
+    colOfPrim=int(colOfPrim)
+    global answers
+    if colOfPrim<=0:
+        primData()
+        return redirect(url_for('.answer'))
+    
+    
+    if request.method=='POST' and colOfPrim!=6:
+        answer=int(custom_eval(truePrim))
+        userA=request.form['YourAnswer']
+        
+        if userA != '' and userA not in list(string.ascii_letters):
+            userA=int(userA)
+            if answer==userA:
+                answers+=1
+        else:
+            redirect(url_for('.BRUH',numsInPrim=numsInPrim,znaki=znaki,colOfPrim=colOfPrim))
+    znaki=normilizer(list(znaki))
+    numsInPrim=int(numsInPrim)    
+    colOfPrim-=1   
     prim=list()
-    znak=['+','-']
     for i in range(1,numsInPrim+1,2):
         prim.append(str(randint(1,100)))
-        prim.append(str(choice(znak)))
+        prim.append(choice(znaki))
+        if prim[-1]=="^":
+            prim.append(str(randint(1,3)))
+            prim.append(choice(znaki[:-1]))
     prim.pop(-1)
     truePrim="".join(prim)
-    answer=eval(truePrim)
-    
-    return render_template('display.html',Prim=truePrim,ans=answer,goodAnswer=answers,col=colOfPrim)
 
-@app.route('/normalLevel')
-def normalLevel():
-    global answers, colOfPrim
-    if colOfPrim==0:
-        print(request.form.get("superAnswer"))
-        return render_template('answer.html',answer=request.form.get("superAnswer"))
-    answers+=1
-    colOfPrim-=1
-    numsInPrim = randint(3,11)
-    prim=list()
-    znak=['+','-','*','/']
-    for i in range(1,numsInPrim+1,2):
-        prim.append(str(randint(1,100)))
-        prim.append(str(choice(znak)))
-    prim.pop(-1)
-    truePrim="".join(prim)
-    answer=eval(truePrim)
+    answer=int(custom_eval(truePrim))
     
-    return render_template('display.html',Prim=truePrim,ans=answer,goodAnswer=answers,col=colOfPrim)
+    return render_template('display.html',Prim=truePrim,znaki=znaki,numsInPrim=numsInPrim,colOfPrim=colOfPrim,truePrim=truePrim)
 
-@app.route('/hardLevel')
-def hardLevel():
-    global answers, colOfPrim
-    if colOfPrim==0:
-        print(request.form.get("superAnswer"))
-        return render_template('answer.html',answer=request.form.get("superAnswer"))
-    answers+=1
-    colOfPrim-=1
-    numsInPrim = randint(3,16)
-    prim=list()
-    znak=['+','-','*','/','**']
-    for i in range(1,numsInPrim+1,2):
-        prim.append(str(randint(1,100)))
-        prim.append(str(choice(znak)))
-    prim.pop(-1)
-    truePrim="".join(prim)
-    answer=eval(truePrim)
+
+@app.route('/primdata')
+def primData():
+
+    primData={
+        'answers':answers
+    }
+    return jsonify(primData)
+
+def normilizer(foo):
+    valid_operators = {'+', '-', '*', '÷', '^'}
+    result = [x for x in foo if x in valid_operators]
+    return result
+
+
+
+def finderOfIndex(baz,foo):
+    if baz in foo:
+        return foo.index(baz)
+    else:
+        return None
+
+def custom_eval(expression):
+    mapping = {
+        '÷': '/',
+        '^': '**' 
+
+    }
     
-    return render_template('display.html',Prim=truePrim,ans=answer,goodAnswer=answers,col=colOfPrim)
+    # Замена символов с помощью map
+    for key, value in mapping.items():
+        expression = expression.replace(key, value)
+    
+    # Выполнение выражения после замены
+    result = eval(expression)
+    return result
 
+
+@app.route('/log/<username>/<password>')
+def log(username, password):
+    api_url = 'http://127.0.0.1:5000/data'
+    response = requests.get(api_url)
+    
+    data = response.json()
+    
+    if username in data['users'] and password == data['passwords'][data['users'].index(username)]:
+        session['username'] = username
+        return redirect(url_for('index'))
+    return redirect(url_for('logun'))
+
+@app.route('/registration', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        
+
+        username = request.form['username']
+        password = request.form['password']
+        mail = request.form['mail']+'@'+request.form['mailmail']
+        data={
+        'users':username,
+        'passwords':password,
+        'mail':mail,
+        'totalAnswers':0
+        }
+        session['username'] = username
+
+        return redirect(url_for('index')), jsonify(data)
+    return render_template('registration.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
